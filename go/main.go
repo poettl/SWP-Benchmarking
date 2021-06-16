05377f4a138dcf639a5d6de51d6abc8eaef8318a
Peter Öttl <peter.oettl@allaboutapps.at>
2021-06-16 17:43:19 +0200

    print

--- go/main.go
+++ go/main.go
@@ -32,23 +32,34 @@ func main() {
 		radixTimes = append(radixTimes, endradixSort.Sub(radixSort))
 	}
 	fmt.Println()
-	fmt.Println()
 	end := time.Now()
+	sumB, avgB := calcAvg(bubbleTimes)
+	sumI, avgI := calcAvg(insertionTimes)
+	sumR, avgR := calcAvg(radixTimes)
+
 	fmt.Println("Duration: ", end.Sub(start))
 	fmt.Println("Array Length: ", 10000)
-
-	fmt.Println("Bubblesort avg duration:", calcAvg(bubbleTimes))
-	fmt.Println("Inseration avg duration:", calcAvg(insertionTimes))
-	fmt.Println("Radix avg duration:", calcAvg(radixTimes))
+	fmt.Println()
+	fmt.Println("--- Bubble Sort ---")
+	fmt.Println("Total: ", sumB)
+	fmt.Println("Avg: ", avgB)
+	fmt.Println()
+	fmt.Println("--- Inseration Sort ---")
+	fmt.Println("Total: ", sumI)
+	fmt.Println("Avg: ", avgI)
+	fmt.Println()
+	fmt.Println("--- Radix Sort ---")
+	fmt.Println("Total: ", sumR)
+	fmt.Println("Avg: ", avgR)
 
 }
 
-func calcAvg(arr []time.Duration) time.Duration {
+func calcAvg(arr []time.Duration) (time.Duration, time.Duration) {
 	sum := float64(0)
 	for _, item := range arr {
 		sum += float64(item.Nanoseconds())
 	}
 	avg := sum / float64(len(arr))
-	return time.Duration(avg)
+	return time.Duration(sum), time.Duration(avg)
 
 }
